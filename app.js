19bb009ec6c3477b213edce107f37df503eb5c15
samhamm <samhamm@gmail.com>
2015-10-14 11:22:45 -0700

    after class

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 var data = [
   {
     value: 25,
-    label: 'Alex',
+    label: 'Alex the Scottish Sensation',
     color: '#811BD6',
     highlight: '#811B33'
   },
   {
-    value: 10,
-    label: 'Scott',
+    value: 5,
+    label: 'Scott the Alexish Aberration',
     color: '#9CBABA',
     highlight: '#9CBA99'
   },
   {
-    value: 30,
-    label: 'Emily',
+    value: 40,
+    label: 'Emily the Extraordinarily Ectoplasmic',
     color: '#D18177',
     highlight: '#D18133'
   },
   {
-    value : 35,
-    label: 'Bella',
+    value : 45,
+    label: 'Bella Bella Bella!!!',
     color: '#6AE128',
     highlight: '#6AE199'
   }
@@ -28,7 +28,7 @@ var data = [
 var context = document.getElementById('skills').getContext('2d');
 var skillsChart = new Chart(context).Doughnut(data, {
     //Number - Amount of animation steps
-    animationSteps : 300,
+    animationSteps : 200,
     //String - Animation easing effect
     animationEasing : "easeOutBounce",
     //Boolean - Whether we animate the rotation of the Doughnut
